6f1f7c87851b4be2f8969e010be81d2b0a6cbe9d
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-07 20:19:33 -0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -146,9 +146,11 @@ MAILERS = {
     },
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://misigma.infinityfree.me",
+]
 
 CORS_ALLOW_METHODS = [
     "DELETE",
