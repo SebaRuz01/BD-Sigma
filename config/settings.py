4ea853071ec36710c2c284c82aeecd38cc73bc85
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-07 20:35:49 -0300

    Add import for os module in settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from decouple import config as env
 from pathlib import Path
 from datetime import timedelta
