a39c4fd42b6cffb10c78662c66cbdcf6d938980c
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-09 00:41:21 -0300

    Change email configuration to use SSL
    Updated email settings to use SSL instead of TLS.

--- config/settings.py
+++ config/settings.py
@@ -135,10 +135,11 @@ STATIC_URL = 'static/'
 # Email (Configurado para usar Resend vía SMTP de forma nativa en Django)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sebaruz2004@gmail.com' 
-EMAIL_HOST_PASSWORD = 'nbnfsegcwsyblffs' 
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_HOST_USER = 'sebaruz2004@gmail.com'
+EMAIL_HOST_PASSWORD = 'nbnfsegcwsyblffs'
 DEFAULT_FROM_EMAIL = 'SIGMA Taller <sebaruz2004@gmail.com>'
 
 
