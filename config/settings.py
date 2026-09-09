d4f2e259a03033884182103eb941237ac326e165
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-09 00:23:40 -0300

    Fix formatting of EMAIL_HOST_PASSWORD

--- config/settings.py
+++ config/settings.py
@@ -138,7 +138,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'sebaruz2004@gmail.com' 
-EMAIL_HOST_PASSWORD = 'nbnf segc wsyb lffs' 
+EMAIL_HOST_PASSWORD = 'nbnfsegcwsyblffs' 
 DEFAULT_FROM_EMAIL = 'SIGMA Taller <sebaruz2004@gmail.com>'
 
 
