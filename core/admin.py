ec611a6f935cdce1c2b6fd24cc39efe47f8e0e54
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-15 01:22:20 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -10,10 +10,7 @@ admin.site.register(Modulo)
 admin.site.register(ModuloContratado)
 admin.site.register(Tecnico)
 admin.site.register(Repuesto)
-
 @admin.register(OrdenTrabajo)
 class OrdenTrabajoAdmin(admin.ModelAdmin):
-    # Cambiamos 'cliente_nombre' por 'cliente' que es el campo real del modelo original
-    list_display = ['id', 'codigo_seguimiento', 'equipo', 'cliente', 'estado']
-
+    list_display = ['id', 'codigo_seguimiento', 'equipo', 'cliente_nombre', 'estado']
 admin.site.register(OrdenRepuesto)
