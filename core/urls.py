e573ab728fa1d29e656066dd2bd1edd950109af4
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-15 01:23:34 -0300

    Refactor URL routing and remove unused imports

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path, include  
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import (
     TallerViewSet, UsuarioViewSet,
     ModuloViewSet, ModuloContratadoViewSet,
     TecnicoViewSet, RepuestoViewSet,
     OrdenTrabajoViewSet, OrdenRepuestoViewSet,
-    OrdenPublicaView, LoginClienteViewSet
+    OrdenPublicaView
 )
 
 router = DefaultRouter()
@@ -13,13 +13,11 @@ router.register(r'talleres', TallerViewSet)
 router.register(r'usuarios', UsuarioViewSet)
 router.register(r'modulos', ModuloViewSet)
 router.register(r'modulos-contratados', ModuloContratadoViewSet)
+router.register(r'ordenes-repuestos', OrdenRepuestoViewSet)
 router.register(r'tecnicos', TecnicoViewSet, basename='tecnico')
 router.register(r'repuestos', RepuestoViewSet, basename='repuesto')
 router.register(r'ordenes', OrdenTrabajoViewSet, basename='orden')
-router.register(r'ordenes-repuestos', OrdenRepuestoViewSet)
-router.register(r'logincliente', LoginClienteViewSet, basename='logincliente')
 
-urlpatterns = [
-    path('api/', include(router.urls)), # <--- Aquí es donde se usa 'include'
-    path('api/publico/ordenes/<str:codigo>/', OrdenPublicaView.as_view()),
+urlpatterns = router.urls + [
+    path('publico/ordenes/<str:codigo>/', OrdenPublicaView.as_view()),
 ]
