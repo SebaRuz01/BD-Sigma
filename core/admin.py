6d7f4a58e658b54e31fda91de57df1dac19a05db
SebaRuz01 <sebaruz2004@gmail.com>
2026-09-17 20:41:00 -0300

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import (
     Taller, Usuario, Modulo, ModuloContratado,
-    Tecnico, Repuesto, OrdenTrabajo, OrdenRepuesto
+    Tecnico, Repuesto, OrdenTrabajo, OrdenRepuesto,
+    Comuna, Vehiculo
 )
+
+
+admin.site.register(Comuna)
+admin.site.register(Vehiculo)
 
 admin.site.register(Taller)
 admin.site.register(Usuario)
@@ -10,7 +15,11 @@ admin.site.register(Modulo)
 admin.site.register(ModuloContratado)
 admin.site.register(Tecnico)
 admin.site.register(Repuesto)
+
 @admin.register(OrdenTrabajo)
 class OrdenTrabajoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'codigo_seguimiento', 'equipo', 'cliente', 'estado', 'fecha_recepcion')
+    # Actualizado con 'vehiculo' en lugar de 'equipo' y 'cliente'
+    list_display = ('id', 'codigo_seguimiento', 'vehiculo', 'estado', 'fecha_recepcion')
+    search_fields = ('codigo_seguimiento', 'vehiculo__patente', 'vehiculo__modelo')
+
 admin.site.register(OrdenRepuesto)
